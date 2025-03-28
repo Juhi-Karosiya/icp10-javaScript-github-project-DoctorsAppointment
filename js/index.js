701fb65bc68c80ad6a8f1dc994d7dfb3ff162004
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-03-28 16:22:18 +0530

    code modified

--- js/index.js
+++ js/index.js
@@ -1,1 +1,157 @@
+function ImageChange(num) {
+    const defaultMessage = document.getElementById("main-container");
+
+    if (num == 1) {
+      defaultMessage.innerHTML = ` <p class="main-paragraph">"At MediCare, I've found not only a fulfilling career but also a second family. The supportive and nurturing atmosphere cultivated by our Chairman has truly inspired me and my Collegues."</p>
+    <p class="para-names">VIJAY MHATRE</p>`;
+      document.getElementById("filledCircle1").src = "./images/red.png";
+      document.getElementById("filledCircle2").src = "./images/grey.png";
+      document.getElementById("filledCircle3").src = "./images/grey.png";
+      document.getElementById("filledCircle4").src = "./images/grey.png";
+      document.getElementById("filledCircle5").src = "./images/grey.png";
+      document.getElementById("filledCircle6").src = "./images/grey.png";
+      document.getElementById("filledCircle7").src = "./images/grey.png";
+      document.getElementById("filledCircle8").src ="./images/grey.png";
+      document.getElementById("filledCircle9").src = "./images/grey.png";
+      document.getElementById("filledCircle10").src = "./images/grey.png";
+    } 
+    
+    else if (num == 2) {
+      defaultMessage.innerHTML = `<p class="main-paragraph">"Working At MediCare Hub for over a decade, I've had a fulfilling experience, with diverse roles and projects that contributed to personal and professional growth."</p>
+       <p class="para-names">JAGMOHAN SINGH RISHI</p> `;
+      document.getElementById("filledCircle1").src = "./images/grey.png";
+      document.getElementById("filledCircle2").src = "./images/red.png"
+      document.getElementById("filledCircle3").src = "./images/grey.png";
+      document.getElementById("filledCircle4").src ="./images/grey.png";
+      document.getElementById("filledCircle5").src = "./images/grey.png"
+      document.getElementById("filledCircle6").src = "./images/grey.png"
+      document.getElementById("filledCircle7").src = "./images/grey.png";
+      document.getElementById("filledCircle8").src = "./images/grey.png";
+      document.getElementById("filledCircle9").src = "./images/grey.png";
+      document.getElementById("filledCircle10").src = "./images/grey.png";
+    } 
+    
+    else if (num == 3) {
+      defaultMessage.innerHTML = `<p class="main-paragraph">"As the leader of the Drug Regulatory Affairs function for the Pharmaceuticals Bussiness at our company, my primary respnsibility is to guide the organization through the regulatory process ." </p> 
+      <p class="para-names">ZOHER SIHORWALA</p>`;
+      document.getElementById("filledCircle1").src ="./images/grey.png";
+      document.getElementById("filledCircle2").src = "./images/grey.png";
+      document.getElementById("filledCircle3").src = "./images/red.png";
+      document.getElementById("filledCircle4").src = "./images/grey.png";
+      document.getElementById("filledCircle5").src = "./images/grey.png";
+      document.getElementById("filledCircle6").src = "./images/grey.png";
+      document.getElementById("filledCircle7").src ="./images/grey.png";
+      document.getElementById("filledCircle8").src = "./images/grey.png";
+      document.getElementById("filledCircle9").src = "./images/grey.png";
+      document.getElementById("filledCircle10").src ="./images/grey.png";
+    } 
+    
+    else if (num == 4) {
+      defaultMessage.innerHTML = `<p class="main-paragraph">"My ten year tenure with MediCare Hub, a global pioneer in pharmaceuticals and biotechnology, has been a remarkable journey of growth and learning." </p> 
+      <p class="para-names">PRAVIN SAMBHARE</p>`;
+
+      document.getElementById("filledCircle1").src ="./images/grey.png";
+      document.getElementById("filledCircle2").src ="./images/grey.png";
+      document.getElementById("filledCircle3").src = "./images/grey.png";
+      document.getElementById("filledCircle4").src = "./images/red.png";
+      document.getElementById("filledCircle5").src = "./images/grey.png";
+      document.getElementById("filledCircle6").src = "./images/grey.png";
+      document.getElementById("filledCircle7").src = "./images/grey.png";
+      document.getElementById("filledCircle8").src = "./images/grey.png";
+      document.getElementById("filledCircle9").src = "./images/grey.png";
+      document.getElementById("filledCircle10").src = "./images/grey.png";
+    } 
+    
+    else if (num == 5) {
+      defaultMessage.innerHTML = `<p class="main-paragraph">"Every day, I am a part of zero ego team thats pushing the boundaries of science to develop new antibiotics that can save lives." </p> 
+      <p class="para-names">DR. VINOD AHIRRAO</p>`;
+      document.getElementById("filledCircle1").src ="./images/grey.png";
+      document.getElementById("filledCircle2").src = "./images/grey.png";
+      document.getElementById("filledCircle3").src = "./images/grey.png";
+      document.getElementById("filledCircle4").src = "./images/grey.png";
+      document.getElementById("filledCircle5").src = "./images/red.png";
+      document.getElementById("filledCircle6").src = "./images/grey.png";
+      document.getElementById("filledCircle7").src = "./images/grey.png";
+      document.getElementById("filledCircle8").src = "./images/grey.png";
+      document.getElementById("filledCircle9").src = "./images/grey.png";
+      document.getElementById("filledCircle10").src = "./images/grey.png";
+    } 
+    
+    else if (num == 6) {
+      defaultMessage.innerHTML = `<p class="main-paragraph">"Relocating from Delhi to Chattrapati Sambhaji nagar was initially challenging, but the welcoming environment and supportive colleagues make the transition seamless." </p> 
+      <p class="para-names">AISHWARYA NAIR</p>`;
+      document.getElementById("filledCircle1").src ="./images/grey.png";
+      document.getElementById("filledCircle2").src = "./images/grey.png";
+      document.getElementById("filledCircle3").src = "./images/grey.png";
+      document.getElementById("filledCircle4").src ="./images/grey.png";
+      document.getElementById("filledCircle5").src = "./images/grey.png";
+      document.getElementById("filledCircle6").src = "./images/red.png";
+      document.getElementById("filledCircle7").src = "./images/grey.png";
+      document.getElementById("filledCircle8").src = "./images/grey.png";
+      document.getElementById("filledCircle9").src = "./images/grey.png"
+      document.getElementById("filledCircle10").src ="./images/grey.png";
+    }
+    
+    else if (num == 7){
+      defaultMessage.innerHTML = `<p class="main-paragraph">"One of the things I appreciate most about working at MediCare Hub is the passion of every team member for top quality scientific work." </p> 
+      <p class="para-names">RAJESH CHAVAN</p>`;
+      document.getElementById("filledCircle1").src = "./images/grey.png";
+      document.getElementById("filledCircle2").src ="./images/grey.png";
+      document.getElementById("filledCircle3").src = "./images/grey.png";
+      document.getElementById("filledCircle4").src = "./images/grey.png";
+      document.getElementById("filledCircle5").src = "./images/grey.png";
+      document.getElementById("filledCircle6").src ="./images/grey.png";
+      document.getElementById("filledCircle7").src = "./images/red.png";
+      document.getElementById("filledCircle8").src = "./images/grey.png";
+      document.getElementById("filledCircle9").src ="./images/grey.png";
+      document.getElementById("filledCircle10").src = "./images/grey.png";
+    }
+    
+    else if (num == 8){
+      defaultMessage.innerHTML = `<p class="main-paragraph">"Every day, I am inspired by the privilege of working alongside a team of seasoned and unwaveringly dedicated scientists." </p> 
+      <p class="para-names">SNEHAL PALWE</p>`;
+      document.getElementById("filledCircle1").src = "./images/grey.png";
+      document.getElementById("filledCircle2").src = "./images/grey.png";
+      document.getElementById("filledCircle3").src = "./images/grey.png";
+      document.getElementById("filledCircle4").src = "./images/grey.png";
+      document.getElementById("filledCircle5").src = "./images/grey.png";
+      document.getElementById("filledCircle6").src = "./images/grey.png";
+      document.getElementById("filledCircle7").src = "./images/grey.png";
+      document.getElementById("filledCircle8").src = "./images/red.png";
+      document.getElementById("filledCircle9").src = "./images/grey.png";
+      document.getElementById("filledCircle10").src = "./images/grey.png";
+  }
+  
+  else if (num == 9){
+    defaultMessage.innerHTML = `<p class="main-paragraph">"The collaborative environment, continuous learning opportunities, and the chance to work on cutting-edge technology and digital atomization have truly enhanced my professional growth." </p> 
+      <p class="para-names">PRADEEP MISHRA</p>`;
+      document.getElementById("filledCircle1").src = "./images/grey.png";
+      document.getElementById("filledCircle2").src = "./images/grey.png";
+      document.getElementById("filledCircle3").src = "grey.png";
+      document.getElementById("filledCircle4").src = "grey.png";
+      document.getElementById("filledCircle5").src = "grey.png";
+      document.getElementById("filledCircle6").src = "grey.png";
+      document.getElementById("filledCircle7").src = "grey.png";
+      document.getElementById("filledCircle8").src = "grey.png";
+      document.getElementById("filledCircle9").src = "./images/red.png";
+      document.getElementById("filledCircle10").src = "grey.png";
+  }else if (num == 10){
+    defaultMessage.innerHTML = `<p class="main-paragraph">"My Journey at MediCare has been a transformative experience that has
+    shaped my professional and personal growth." </p> 
+      <p class="para-names">LCS SRI VISHNU</p>`;
+      document.getElementById("filledCircle1").src = "grey.png";
+      document.getElementById("filledCircle2").src = "grey.png";
+      document.getElementById("filledCircle3").src = "grey.png";
+      document.getElementById("filledCircle4").src = "grey.png";
+      document.getElementById("filledCircle5").src = "grey.png";
+      document.getElementById("filledCircle6").src = "grey.png";
+      document.getElementById("filledCircle7").src = "grey.png";
+      document.getElementById("filledCircle8").src = "grey.png";
+      document.getElementById("filledCircle9").src = "grey.png";
+      document.getElementById("filledCircle10").src ="./images/red.png";
+
+  }
+}
+
+
 
